459aa30ae6e5314697f82cd63cb681d306ac1f2a
GrittyGitty <58652339+GrittyGitty@users.noreply.github.com>
2020-05-06 17:41:34 +0300

    history and feelings insert now work.

--- weather/utilities/userWeatherSchema.js
+++ weather/utilities/userWeatherSchema.js
@@ -1,21 +1,54 @@
 const { Schema } = require('mongoose');
 const { weather } = require('../mongodbConnection');
 
+
+const feelingsSchema = new Schema({
+    city: String,
+    time: Date,
+    feeling: String
+});
 const userWeatherSchema = new Schema({
     username: String,
     history: [String],
-    feelings: [{ city: String, time: Date }]
+    feelings: [feelingsSchema]
 });
+
 
 const UserWeather = weather.model('UserHistory', userWeatherSchema);
 
-const userWeather = new UserWeather({
-    username: "aaa",
-    history: ["london", "paris"],
-    feelings: [
-        { city: "london", time: Date.now() },
-        { city: "paris", time: Date.now() }
-    ]
-}).save();
+function createUser(username, history = [], feelings) {
+    return new UserWeather({ username, history, feelings })
+}
+
+async function getOrCreateRecord(username) {
+    let user = await UserWeather.findOne({ username });
+    if (user)
+        return user;
+    else {
+        user = createUser(username);
+        user.save();
+    }
+    return user;
+}
+
+
+async function updateHistory(username, searchItem) {
+    const user = await getOrCreateRecord(username);
+    user.history = [...user.history, searchItem];
+    user.save();
+}
+
+async function updateFeelings(username, city, time, feeling) {
+    const user = await getOrCreateRecord(username);
+    user.feelings = [...user.feelings, { city, time, feeling }]
+    user.save();
+}
+
+
+module.exports = {
+    updateHistory,
+    updateFeelings
+}
+
 
 
